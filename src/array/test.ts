d0d07e3b370d0cbcdc1af0a58a9d9418c87db331
tongsiyuan <1084350607@qq.com>
2021-02-05 17:30:30 +0800

    Add tools 2021.2.5

--- src/array/test.ts
+++ src/array/test.ts
@@ -1,8 +1,5 @@
-import {ArrayIterateeFunc, NullAndUndefined, ObjIterateeFunc} from "../interface";
-
 export function arrayTest(array): void {
   if (!Array.isArray(array)) {
     throw new TypeError('The first parameter must be an array')
   }
-  return
 }
